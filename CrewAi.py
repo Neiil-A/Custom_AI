c463fa8487d17ee2338e8f8529e5c04dc8050ede
Neiil <apshankarneiil@gmail.com>
2024-08-05 22:41:49 -0400

    Update and rename Tasks.py to CrewAi.py

---
+++ CrewAi.py
@@ -0,0 +1,18 @@
+# crewAi.py
+
+class Crew:
+    def __init__(self, agents, tasks, process):
+        self.agents = agents
+        self.tasks = tasks
+        self.process = process
+
+    def kickoff(self):
+        # Implementation for starting the content generation process
+        pass
+
+class Process:
+    parallel = "parallel"
+    # Implementation of Process class
+
+
+   
